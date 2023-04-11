b57c0ba218b0855eb869f8d0cd3892c709a59f54
Rogério Fontes <fontestz@gmail.com>
2023-04-11 17:30:23 -0300

    :rocket: adicao do menu como component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
+import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import buttonStyles from "../styles/Button.module.css";
 import messageStyles from "../styles/Message.module.css";
+import Navbar from "../components/navbar";
 
 function HomePage() {
     return (
         <div className={styles.container}>
+            <Head>
+                <title>NextJS Blog with Butter CMS</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+        
             Hello Word<br />
             <div className={messageStyles.success}>
                 Something has been successfully done.
